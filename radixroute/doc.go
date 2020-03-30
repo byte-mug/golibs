175bde353d4b37c38bec5d5644bd71bca4f17492
Simon Schmidt <simon.schmidt@htp-tel.de>
2020-03-30 16:04:55 +0200

    radix-route

--- radixroute/doc.go
+++ radixroute/doc.go
@@ -1,6 +1,5 @@
-MIT License
-
-Copyright (c) 2017-2020 Simon Schmidt
+/*
+Copyright (c) 2020 Simon Schmidt
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -19,3 +18,12 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*/
+
+
+/*
+A radix-tree router for URLs/Paths.
+*/
+package radixrouter
+
+
